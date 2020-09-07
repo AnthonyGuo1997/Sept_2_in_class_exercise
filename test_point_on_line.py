f394fcd25d237861d40c833e8871af18d95f3af6
AnthonyGuo1997 <boyu.guo@duke.edu>
2020-09-07 05:48:13 -0400

    Added unit test function for get_slope() in the testing file

--- test_point_on_line.py
+++ test_point_on_line.py
@@ -3,3 +3,9 @@ def test_point_on_line():
     y_coord = point_on_line((0,0),(1,1),3)
     expected = 3
     assert y_coord == expected
+
+def test_get_slope():
+    from point_on_line import get_slope
+    calc_slope = get_slope((1,1), (2,3))
+    expected = 2
+    assert calc_slope = expected
