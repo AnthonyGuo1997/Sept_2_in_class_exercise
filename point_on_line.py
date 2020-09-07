ddfeccd98fb9014a87c1cd553f32a74d6c29d9cf
AnthonyGuo1997 <boyu.guo@duke.edu>
2020-09-07 05:39:41 -0400

    Corrected mistakes in point_on_line() function -- changed indexing syntax from () to []; changed index to start with 0. Updated .gitignore file to include pycache

--- point_on_line.py
+++ point_on_line.py
@@ -1,5 +1,5 @@
 def point_on_line(point_1, point_2, x3):
-    slope = (point_1(2)-point_2(2))/(point_1(1)-point_2(1))
-    intercept = point_1(2)-slope*point_1(1)
+    slope = (point_1[1]-point_2[1])/(point_1[0]-point_2[0])
+    intercept = point_1[1]-slope*point_1[0]
     y3 = x3*slope + intercept
     return y3
