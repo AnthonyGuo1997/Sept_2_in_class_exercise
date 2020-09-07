6b2f8d8127bc9c84d13989f57d081f4db7150632
AnthonyGuo1997 <boyu.guo@duke.edu>
2020-09-07 05:32:55 -0400

    Corrected mistakes in test file. Changed input to two tuples and one int. Fixed assert line

--- test_point_on_line.py
+++ test_point_on_line.py
@@ -1,5 +1,5 @@
 def test_point_on_line():
     from point_on_line import point_on_line
-    y_coord = point_on_line(0,0,1,1,3)
+    y_coord = point_on_line((0,0),(1,1),3)
     expected = 3
-    assert answer == expected
+    assert y_coord == expected
